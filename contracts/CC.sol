5bb272bc4b5b0b925f5f745509b4e586be9194db
julienbrg <julien@strat.cc>
2021-10-07 22:26:23 +0200

    add register

--- contracts/CC.sol
+++ contracts/CC.sol
@@ -6,15 +6,33 @@ import "hardhat/console.sol";
 
 contract CC is ERC20 {
     
-    event Withdrawn(address indexed user, uint indexed amount );
+    address public kiezDAO;
     
-    constructor() ERC20("Magic CC for hackers", "CC") {
-        // _mint(msg.sender, 1000000*10**18);
+    event Withdrawn(address indexed user, uint indexed amount);
+    
+    constructor(address _kiezDAO) ERC20("CC Token", "CC") {
+        kiezDAO = _kiezDAO;
     }
     
-    // mints 50 units on the fly
-    function withdraw(address _beneficiary) public {
-        _mint(_beneficiary, 50*10**18);
+    mapping(address => bool) public memberRegistered;
+	address[] public membersList;
+    
+    // triggered from the DAO contract
+    function withdraw(address _beneficiary, uint256 _amount) public {
+        require(msg.sender == kiezDAO, "CANNOT_MINT");
+        _mint(_beneficiary, _amount*10**18);
         emit Withdrawn (_beneficiary, 50);
+        }
+    
+    // msg.sender gets 1000 units for free // Subject to Sybil attacks
+    function register() public {
+        if (!memberRegistered[msg.sender]) {
+			memberRegistered[msg.sender] = true;
+			membersList.push(msg.sender);
+			_mint(msg.sender, 1000*10**18);
+            emit Withdrawn (msg.sender, 1000);
+		} else {
+		    revert("ALREADY_REGISTERED");
+		}
     }
 }
