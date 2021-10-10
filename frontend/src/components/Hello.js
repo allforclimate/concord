c8a489f7c8c1bae83929dc7feab0c23562966bcd
julienbrg <julien@strat.cc>
2021-10-10 11:59:39 +0200

    add basic call

--- frontend/src/components/Hello.js
+++ frontend/src/components/Hello.js
@@ -1,22 +1,37 @@
 import { useWeb3React } from '@web3-react/core';
+import { Contract, ethers } from 'ethers';
+import ccAddress from '../contracts/contractAddress.json';
+import CC from '../contracts/CC.json';
 
 const Hello = () => {
 
-    const { active, chainId, account, error } = useWeb3React();
+    const inCC = CC;
+    console.log(inCC);
 
-    const register = () => {
+    const { account } = useWeb3React();
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const register = async () => {
+        console.log(ccAddress.CC);
+        console.log(account);
+
+        const signer = provider.getSigner(0);
+        const addr = ccAddress.CC;
+        const abi = CC.abi;
         
-        console.log("yo");
-      };
-    
+        const cc = new Contract(addr, abi, signer);
+        const call = await cc.register();
+    };
+
     return (
         <div className="wallet-info">
-          <p>Click on this button to register as a member of the DAO:</p>
-          <button className="button-standard" onClick={register}>
-          Register
+            <p>Click on this button to register as a member of the DAO:</p>
+            <button className="button-standard" onClick={register}>
+            Register
         </button>
         </div>
-      );
-};
+        );
+    };
 
 export default Hello;
