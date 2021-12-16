cfee99adf8a0940e4522560d1b7230c148e9a469
julienbrg <julien@strat.cc>
2021-12-16 17:17:29 +0100

    add /balance

--- bot/slash/balance.js
+++ bot/slash/balance.js
@@ -2,49 +2,26 @@ const ethers = require('ethers');
 // const { Wallet } = require('ethers');
 const fs = require('fs');
 // const { permlevel, getTodayString } = require('../modules/functions.js');
+const { registeredUsers } = require('../modules/tables.js');
+const { getTokenBalance } = require('../modules/functions.js');
+
+
 
 require('dotenv').config();
 
-// returns the balance of user's wallet
-exports.run = async (interaction, message) => { // eslint-disable-line no-unused-vars
+exports.run = async (client, interaction) => { // eslint-disable-line no-unused-vars
     await interaction.deferReply();
+    const reply = await interaction.editReply("Ping?");
 
-    function getInfuraProvider() {
-        const apiKey = {
-            projectId: process.env.INFURA_PROJECT_ID,
-            projectSecret: process.env.INFURA_PROJECT_SECRET
-        };
-        const provider = new ethers.providers.InfuraProvider(network=process.env.NETWORK, apiKey);
-        return provider;
-    }
-    const provider = getInfuraProvider();
+    const address = registeredUsers.get(interaction.user.id);
+    console.log("address: ", address);
 
-    async function checkMyBalance(message) {
-        try {
-            let wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC); 
-            wallet = wallet.connect(provider);   
-            const abiFile = fs.readFileSync('modules/concordAbi.json');
-            const abi = JSON.parse(abiFile);
-            const addressRaw = fs.readFileSync('modules/concordAddress.json');
-            const addr = JSON.parse(addressRaw);
-            const concord = new ethers.Contract(addr.concord, abi, wallet);
+    const bal = await getTokenBalance(address);
 
-            const userAddr = message.author.address ;
-            const amount = 1;
-            const task = "Gave a hand.";
-            console.log("userAddr: ", userAddr);
 
-            const call = await concord.balanceOf(userAddr);    
-            console.log("call: ", call);
-    
-        } catch (err) {
-            console.error(err);
-        }
-    }
-    
-    const bal = checkMyBalance();
 
-    await interaction.editReply(`Hey ${message.author.address}! What's up? You currently own ${bal} `);
+    await interaction.editReply(`Result: ${bal}`);
+
   };
   
   exports.commandData = {
